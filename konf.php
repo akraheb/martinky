a8aeeab85d28e829a3c7138385a09b5a96fa184b
akraheb <102849037+akraheb@users.noreply.github.com>
2022-04-08 13:23:25 +0200

    Delete konf.php

--- konf.php
+++
@@ -1,7 +0,0 @@
-<div><iframe src="https://www.holidayinfo.sk/sk/Kamera/286102"></iframe></div>
-<div><iframe src="https://www.panorama.sk/sk/webkamera/winter-park-martinky-3/722"></iframe></div>
-<div>
-<?php
-echo phpinfo();
-?>
-</div>
